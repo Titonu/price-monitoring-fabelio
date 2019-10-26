d7341de024cd9c81e54ad7225ebeb106277c391b
Aditya Tito <aditonu@gmail.com>
2019-10-27 06:27:12 +0700

    Initial Commit

---
+++ src/test/java/com/tito/github/pricemonitoring/PriceMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tito.github.pricemonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
